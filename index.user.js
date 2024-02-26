75c611268deb143d51a0bad2f38ec426387b4b3c
feyli <48140698+feeeyli@users.noreply.github.com>
2024-02-26 15:55:00 -0300

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Twitter da Rapaziada
 // @match       https://twitter.com/*
-// @version     2.2
+// @version     2.3
 // @author      feyli
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@1
 // @require     https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
@@ -197,7 +197,7 @@ function run() {
         $(item.el)
           .find("> div:not(:contains(@))")
           .append(
-            `<div data-testid="tdr-icons" style="display:inline-flex;margin-left:2px;">${item.badges
+            `<div data-testid="tdr-icons" style="display:inline-flex;margin-left:2px;vertical-align:middle;">${item.badges
               .map((badge) => {
                 if (badge.name !== "cellbit-logo")
                   return iconImg({
@@ -252,7 +252,7 @@ function run() {
             ".css-1rynq56.r-bcqeeo.r-qvutc0.r-37j5jr.r-1vr29t4.r-1awozwy.r-6koalj.r-1udh08x > span.css-1qaijid.r-bcqeeo.r-qvutc0.r-poiln3"
           )
           .append(
-            `<div data-testid="tdr-icons" style="display:inline-flex;margin-left:2px;">${item.badges
+            `<div data-testid="tdr-icons" style="display:inline-flex;margin-left:2px;vertical-align:middle;">${item.badges
               .map((badge) => {
                 if (badge.name !== "cellbit-logo")
                   return iconImg({
@@ -312,7 +312,7 @@ function run() {
             ".css-1rynq56.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-b88u0q.r-1awozwy.r-6koalj.r-1udh08x.r-3s2u2q > span.css-1qaijid.r-dnmrzs.r-1udh08x.r-3s2u2q.r-bcqeeo.r-qvutc0.r-poiln3"
           )
           .append(
-            `<div data-testid="tdr-icons" style="display:inline-flex;margin-left:2px;">${item.badges
+            `<div data-testid="tdr-icons" style="display:inline-flex;margin-left:2px;vertical-align:middle;">${item.badges
               .map((badge) => {
                 if (badge.name !== "cellbit-logo")
                   return iconImg({
